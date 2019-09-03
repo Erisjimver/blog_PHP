0946ff4f14432fd439ca295a87fe5cf31b3b67ff
erisjimver <erisjinver@gmail.com>
2019-09-03 12:54:02 -0500

    existe un error a la lectura del blog

--- blog.php
+++ blog.php
@@ -8,6 +8,8 @@
 
 	<?php 
 
+	//echo "Inicia la wea";
+
 		$conexion=mysqli_connect("localhost", "root", "", "bbddblog");
 
 		if (!$conexion) {
@@ -16,24 +18,25 @@
 
 			exit();
 		}
-
+		//$consulta="select titulo,fecha,comentario,imagen from cotenido order by fecha desc";
 		$consulta="select * from cotenido order by fecha desc";
 
-
+echo "K paso aqui";
 		if($resultado=mysqli_query($conexion, $consulta)){
-
+echo "Aki";
 			while ($registro=mysqli_fetch_assoc($resultado)) {
-				echo "<h3>" . $registro["titulo"] . "<h3>";
-				echo "<h4> " . $registro["fecha"] . "<h4>";
-				echo "<div style='width=400px'>" . $registro["comentario"] . "</div> <br>";
+				echo "<h3>" . $registro["titulo"] . "</h3>";
+				echo "<h4>" . $registro["fecha"] . "</h4>";
+				echo "<div style=width:'400px'>" . $registro["comentario"] . "</div> <br>";
 				if($registro["imagen"]!=[""]){
-					echo "<img src='imagenes/" . $registro['imagen'] . "' width='300px' />";
+					echo "<img src='imagenes/" . $registro['imagen'] . "' width:'300px' />";
 				}
 				echo "<hr/>";
 
 			}
+			echo "K paso aki de nuevo";
 		}
-
+//echo "Termina la wea";
 
 	?>
 
