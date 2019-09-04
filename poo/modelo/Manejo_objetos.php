8e46a2b0379e35229a5af982e655278948ff811e
erisjimver <erisjinver@gmail.com>
2019-09-03 22:44:07 -0500

    creando archivo manejo objeto

---
+++ poo/modelo/Manejo_objetos.php
@@ -0,0 +1,64 @@
+<?php 
+
+/**
+ * 
+ */
+
+include ("Objeto_blog.php");
+
+class Manejo_objeto{
+	
+
+
+		private $conexion;
+
+		public function __construct($conexion){
+
+			$this->setConexion($conexion);
+
+		}
+
+		public function setConexion(PDO $conexion){
+
+			$this->conexion=$conexion;
+
+		}
+
+		public function getContenidoPorFecha(){
+
+			$matriz=array();
+
+			$contador=0;
+
+			$resultado=this->conexion->query("select * from contenido order by desc");
+
+			while ($registro=$resultado)->fetch(PDO::FETCH_ASSOC) {
+				
+				$blog=new Objeto_Blog();
+
+				$blog->setID($registro["id"]);
+				$blog->setTitulo($registro["titulo"]);
+				$blog->setFecha($registro["fecha"]);
+				$blog->setComentarios($registro["comentario"]);
+				$blog->setImagen($registro["imagen"]);
+
+				$matriz[$contador]=$blog;
+				$contador++;
+			}
+
+			return $matriz;
+
+		}
+
+		public function insertaCOntenido(Objeto_blog $blog){
+
+			$sql="insert into contenido (titulo,fecha,comentario,imagen) values ('$blog->getTitulo','$blog->getFecha','$blog->getComentarios','$blog->getImagen')";
+			$this->$conexion->exec($sql);
+		}
+		
+	
+}
+
+
+
+?>
